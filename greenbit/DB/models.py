e4863695cf4547a6a13f324b9f5bfddb7109fefe
Bob Fashan <danilgrachev1992@gmail.com>
2024-02-24 18:57:08 +0300

    base crud orm

---
+++ greenbit/DB/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from DB.database import Base, str_256
+import datetime
+from typing import Annotated
+from sqlalchemy import text
+
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[
+    datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))
+]
+updated_at = Annotated[
+    datetime.datetime,
+    mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.utcnow,
+    ),
+]
+
+
+class UserOrm(Base):
+    __tablename__ = "user"
+
+    id: Mapped[intpk]
+    user_name: Mapped[str_256] = mapped_column(nullable=False)
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
+
+
+class UserAuthorm(Base):
+    __tablename__ = "auth"
+    user_id: 
+    email: Mapped[str_256] = mapped_column(nullable=False)
+    password: Mapped[str_256] = mapped_column(nullable=False)
+    
