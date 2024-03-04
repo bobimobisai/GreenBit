d69381c2813180940cd517a87fcfcb7f8c708663
Bob Fashan <danilgrachev1992@gmail.com>
2024-03-04 19:27:50 +0300

    update

---
+++ greenbit/app/main.py
@@ -0,0 +1,27 @@
+# from faker import Faker
+# from DB.core_crud import AsyncCore
+# from DB.orm_crud import AsyncOrm
+# import asyncio
+
+
+# def get_user_data():
+#     fake = Faker()
+#     while True:
+#         data = {
+#             "user_name": fake.first_name(),
+#             "email": fake.email(),
+#             "password": fake.password(),
+#         }
+#         yield data
+
+
+# user_data = get_user_data()
+
+
+# async def main():
+#     await AsyncOrm.insert_user_auth(data=next(user_data))
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
