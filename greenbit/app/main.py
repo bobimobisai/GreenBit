9ad92e9e8cd52b016ca56f7f3f69c6148fcca0e2
Bob Fashan <danilgrachev1992@gmail.com>
2024-03-13 17:41:52 +0300

    new_host"
    git commit -m new_host
    
    git commit -m 'new_host

--- greenbit/app/main.py
+++ greenbit/app/main.py
@@ -1,27 +1,27 @@
-# from faker import Faker
-# from DB.core_crud import AsyncCore
-# from DB.orm_crud import AsyncOrm
-# import asyncio
+from faker import Faker
+from DB.core_crud import AsyncCore
+from DB.orm_crud import AsyncOrm
+import asyncio
 
 
-# def get_user_data():
-#     fake = Faker()
-#     while True:
-#         data = {
-#             "user_name": fake.first_name(),
-#             "email": fake.email(),
-#             "password": fake.password(),
-#         }
-#         yield data
+def get_user_data():
+    fake = Faker()
+    while True:
+        data = {
+            "user_name": fake.first_name(),
+            "email": fake.email(),
+            "password": fake.password(),
+        }
+        yield data
 
 
-# user_data = get_user_data()
+user_data = get_user_data()
 
 
-# async def main():
-#     await AsyncOrm.insert_user_auth(data=next(user_data))
+async def main():
+    await AsyncOrm.create_tables()
+    await AsyncOrm.insert_user_auth(data=next(user_data))
 
 
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
+if __name__ == "__main__":
+    asyncio.run(main())
